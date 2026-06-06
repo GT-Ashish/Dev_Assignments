2ada5f973df8fa7f0c5c08a84f8ce46da0a49b74
Ashish Murarka <murarkaashish6294@gmail.com>
2026-06-06 23:29:29 +0530

    chore: organize JS/CSS folders and add Lesson 11 assignment solutions

---
+++ JavaScript/scripts/todo-list.js
@@ -0,0 +1,22 @@
+const input = document.querySelector('.js-input');
+const addButton = document.querySelector('.js-add-button');
+let array = [];
+addButton.addEventListener('click',() =>{
+    array.push(input.value);
+    input.value = null;
+    console.log(array);
+});
+const input2 = document.querySelector('.js-input-2');
+const addButton2 = document.querySelector('.js-add-button-2');
+const listText = document.querySelector('.js-list-text');
+let array2 = [];
+let acc = '';
+listText.innerHTML = acc;
+addButton2.addEventListener('click',() =>{
+    // array2.push(input2.value);
+    // array2.push('<br>');
+    // acc = array2.join("");
+    acc += `${input2.value} <br>`;
+    input2.value = null;
+    listText.innerHTML = acc;    
+});
